3cd079fecf8503c601935ad2df84e26b6e963cd7
Shridhar V G <116273227+shrisg17@users.noreply.github.com>
2022-12-30 14:41:35 +0530

    Add files via upload

---
+++ MLProject.py
@@ -0,0 +1,19 @@
+# To check the accuracy of model
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+music_data = pd.read_csv('/home/shridhar/Jupyter/music.csv')
+x = music_data.drop(columns = ['genre'])
+y = music_data['genre']
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.2)
+
+model = DecisionTreeClassifier()
+model.fit(x_train,y_train)
+
+predictions = model.predict(x_test)
+
+score = accuracy_score(y_test,predictions)
+print(f'{int(score*100)}%')
